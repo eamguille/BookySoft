7a00bdc706022bc09b7e58cc6ae3e1cd23e3be2b
eamguille <eamguille@gmail.com>
2024-08-15 01:06:31 -0600

    comienzo fornulario principal

--- sistema_gestion_biblioteca/Forms/FrmPrincipal.Designer.cs
+++ sistema_gestion_biblioteca/Forms/FrmPrincipal.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmPrincipal";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            panel1 = new Panel();
+            btnRestaurar = new PictureBox();
+            btnMaximizar = new PictureBox();
+            btnMinimizar = new PictureBox();
+            btnCerrar = new PictureBox();
+            PnMenuVertical = new Panel();
+            PnContenedor = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)btnRestaurar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMaximizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 80, 200);
+            panel1.Controls.Add(btnRestaurar);
+            panel1.Controls.Add(btnMaximizar);
+            panel1.Controls.Add(btnMinimizar);
+            panel1.Controls.Add(btnCerrar);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1300, 35);
+            panel1.TabIndex = 0;
+            // 
+            // btnRestaurar
+            // 
+            btnRestaurar.Image = (Image)resources.GetObject("btnRestaurar.Image");
+            btnRestaurar.Location = new Point(1230, 5);
+            btnRestaurar.Name = "btnRestaurar";
+            btnRestaurar.Size = new Size(25, 25);
+            btnRestaurar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnRestaurar.TabIndex = 14;
+            btnRestaurar.TabStop = false;
+            btnRestaurar.Visible = false;
+            btnRestaurar.Click += btnRestaurar_Click;
+            // 
+            // btnMaximizar
+            // 
+            btnMaximizar.Image = (Image)resources.GetObject("btnMaximizar.Image");
+            btnMaximizar.Location = new Point(1231, 5);
+            btnMaximizar.Name = "btnMaximizar";
+            btnMaximizar.Size = new Size(25, 25);
+            btnMaximizar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnMaximizar.TabIndex = 13;
+            btnMaximizar.TabStop = false;
+            btnMaximizar.Click += btnMaximizar_Click;
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Image = (Image)resources.GetObject("btnMinimizar.Image");
+            btnMinimizar.Location = new Point(1196, 5);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(25, 25);
+            btnMinimizar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnMinimizar.TabIndex = 12;
+            btnMinimizar.TabStop = false;
+            btnMinimizar.Click += btnMinimizar_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(1267, 5);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(25, 25);
+            btnCerrar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnCerrar.TabIndex = 11;
+            btnCerrar.TabStop = false;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // PnMenuVertical
+            // 
+            PnMenuVertical.BackColor = Color.FromArgb(26, 32, 40);
+            PnMenuVertical.Dock = DockStyle.Left;
+            PnMenuVertical.Location = new Point(0, 35);
+            PnMenuVertical.Name = "PnMenuVertical";
+            PnMenuVertical.Size = new Size(220, 615);
+            PnMenuVertical.TabIndex = 1;
+            // 
+            // PnContenedor
+            // 
+            PnContenedor.BackColor = Color.FromArgb(49, 66, 82);
+            PnContenedor.Dock = DockStyle.Fill;
+            PnContenedor.Location = new Point(220, 35);
+            PnContenedor.Name = "PnContenedor";
+            PnContenedor.Size = new Size(1080, 615);
+            PnContenedor.TabIndex = 2;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 650);
+            Controls.Add(PnContenedor);
+            Controls.Add(PnMenuVertical);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmPrincipal";
+            Text = "FrmPrincipal";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)btnRestaurar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMaximizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel PnMenuVertical;
+        private Panel PnContenedor;
+        private PictureBox btnMinimizar;
+        private PictureBox btnCerrar;
+        private PictureBox btnMaximizar;
+        private PictureBox btnRestaurar;
     }
 }
--- sistema_gestion_biblioteca/Forms/FrmPrincipal.cs
+++ sistema_gestion_biblioteca/Forms/FrmPrincipal.cs
@@ -16,5 +16,29 @@ namespace sistema_gestion_biblioteca.Forms
         {
             InitializeComponent();
         }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnRestaurar.Visible = false;
+            btnMaximizar.Visible = true;
+        }
     }
 }
