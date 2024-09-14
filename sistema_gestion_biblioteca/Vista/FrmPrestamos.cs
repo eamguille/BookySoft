aa63f3d077cb4ee6d006deaf6027ddbb1658b45e
ronyv5 <ronnyvillela2018@gmail.com>
2024-09-14 03:53:13 -0600

    Validación 3

--- sistema_gestion_biblioteca/Vista/FrmPrestamos.cs
+++ sistema_gestion_biblioteca/Vista/FrmPrestamos.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using sistema_gestion_biblioteca.Controlador;
 
@@ -36,13 +31,15 @@ namespace sistema_gestion_biblioteca.Forms
             dtFechaInicial.Format = DateTimePickerFormat.Custom;
             dtFechaInicial.CustomFormat = " ";
             cargarHeadersDataGrid();
+
+            // Establecer la fecha máxima en el DateTimePicker
+            dtFechaInicial.MaxDate = DateTime.Now.Date;
         }
 
         private BindingSource enlaceDatos = new BindingSource();
-
         private int index_tabla = -1;
 
-        // METODOS DE SCRUD
+        // MÉTODOS DE CRUD
 
         void cargarCmbLibros()
         {
@@ -51,13 +48,9 @@ namespace sistema_gestion_biblioteca.Forms
             if (lista != null && lista.Count > 0)
             {
                 var pos = lista.Select(elemento => elemento.titulo_libro).ToList();
-
                 pos.Insert(0, "Selecciona el libro");
-
                 cmbLibro.DataSource = pos;
-
                 cmbLibro.SelectedIndex = 0;
-
                 cmbLibro.SelectedIndexChanged += (s, ev) =>
                 {
                     if (cmbLibro.SelectedIndex == 0)
@@ -75,13 +68,9 @@ namespace sistema_gestion_biblioteca.Forms
             if (lista != null && lista.Count > 0)
             {
                 var pos = lista.Select(elemento => elemento.email).ToList();
-
                 pos.Insert(0, "Selecciona el correo");
-
                 cmbUsuario.DataSource = pos;
-
                 cmbUsuario.SelectedIndex = 0;
-
                 cmbUsuario.SelectedIndexChanged += (s, ev) =>
                 {
                     if (cmbUsuario.SelectedIndex == 0)
@@ -95,15 +84,10 @@ namespace sistema_gestion_biblioteca.Forms
         void cargarCmbEstadoPrestamo()
         {
             cmbEstadoPrestamo.DropDownStyle = ComboBoxStyle.DropDownList;
-
             List<String> lista = new List<String> { "En curso", "Devuelto", "Retrasado" };
-
             cmbEstadoPrestamo.Items.Insert(0, "Selecciona el estado");
-
             cmbEstadoPrestamo.Items.AddRange(lista.ToArray());
-
             cmbEstadoPrestamo.SelectedIndex = 0;
-
             cmbEstadoPrestamo.SelectedIndexChanged += (s, ev) =>
             {
                 if (cmbEstadoPrestamo.SelectedIndex == 0)
@@ -124,24 +108,31 @@ namespace sistema_gestion_biblioteca.Forms
         {
             try
             {
-                // Obtenenemos el libro seleccionado
+                // Validación de la fecha de préstamo
+                if (dtFechaInicial.Value.Date > DateTime.Now.Date)
+                {
+                    MessageBox.Show("La fecha de préstamo no puede ser superior a la actual", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Obtenemos el libro seleccionado
                 string nombre_libro = cmbLibro.SelectedItem.ToString();
 
-                // Buscamos en ISBN segun el titulo del libro
+                // Buscamos el ISBN según el título del libro
                 string libroISBN = buscarISBNPorNombreLibro(nombre_libro);
 
-                // Condicion de ISBN encontrado o no (Despues)
-
+                // Guardar el préstamo
                 bool guardado = obj_controlador.agregarPrestamo(cmbLibro.Text, cmbUsuario.Text, dtFechaInicial.Text, lblFechaDevolucion.Text, cmbEstadoPrestamo.Text);
                 if (guardado)
                 {
                     bool estado_libro_actualizar = obj_libro_controlador.actualizarEstadoLibro(libroISBN, "No Disponible");
                     if (estado_libro_actualizar)
                     {
-                        MessageBox.Show("Prestamo ingresado exitosamente", "Tarea exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    } else
+                        MessageBox.Show("Préstamo ingresado exitosamente", "Tarea exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
                     {
-                        MessageBox.Show("Prestamo ingresado exitosamente, pero no se pudo actualizar el estado del libro", "Tarea exitosa (Advertencia)", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        MessageBox.Show("Préstamo ingresado exitosamente, pero no se pudo actualizar el estado del libro", "Tarea exitosa (Advertencia)", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     }
                     ActualizarDataGrid();
                 }
@@ -159,10 +150,9 @@ namespace sistema_gestion_biblioteca.Forms
                 if (index_tabla >= 0)
                 {
                     bool actualizar = obj_controlador.actualizarPrestamo(index_tabla, cmbLibro.Text, cmbUsuario.Text, dtFechaInicial.Text, lblFechaDevolucion.Text, cmbEstadoPrestamo.Text);
-
                     if (actualizar)
                     {
-                        MessageBox.Show("Prestamo actualizado exitosamente", "Tarea exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show("Préstamo actualizado exitosamente", "Tarea exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         ActualizarDataGrid();
                     }
                 }
@@ -177,17 +167,16 @@ namespace sistema_gestion_biblioteca.Forms
         {
             try
             {
-                if (MessageBox.Show("Deseas eliminar el prestamo ?", "Confirmacion de eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                if (MessageBox.Show("¿Deseas eliminar el préstamo?", "Confirmación de eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
                     if (dgPrestamos.SelectedRows.Count > 0)
                     {
                         if (index_tabla >= 0)
                         {
                             bool eliminar = obj_controlador.eliminarPrestamo(index_tabla);
-
                             if (eliminar)
                             {
-                                MessageBox.Show("Prestamo eliminado exitosamente", "Tarea exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                MessageBox.Show("Préstamo eliminado exitosamente", "Tarea exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
                                 ActualizarDataGrid();
                             }
                         }
@@ -219,7 +208,6 @@ namespace sistema_gestion_biblioteca.Forms
             else
             {
                 var busqueda = filtro_buscador.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
                 var listaFiltrada = lista.Where(ele =>
                     busqueda.All(t =>
                         (ele.titulo_libro != null && ele.titulo_libro.Contains(t, StringComparison.OrdinalIgnoreCase)) ||
@@ -237,7 +225,7 @@ namespace sistema_gestion_biblioteca.Forms
             dgPrestamos.DataSource = enlaceDatos;
         }
 
-        // Variable para guardar la fecha estimada de devolucion
+        // Variable para guardar la fecha estimada de devolución
         DateTime fecha_devolucion;
 
         private void dtFechaInicial_ValueChanged(object sender, EventArgs e)
@@ -246,10 +234,19 @@ namespace sistema_gestion_biblioteca.Forms
 
             DateTime fecha_prestamo = dtFechaInicial.Value;
 
-            // Sumamos un mes para la fecha estimada de devolucion
+            // Validación: Comprobar si la fecha seleccionada es mayor que la fecha actual
+            if (fecha_prestamo > DateTime.Now)
+            {
+                MessageBox.Show("La fecha de préstamo no puede ser superior a la actual", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Restablecer la fecha al valor actual
+                dtFechaInicial.Value = DateTime.Now;
+                return;
+            }
+
+            // Sumamos un mes para la fecha estimada de devolución
             fecha_devolucion = fecha_prestamo.AddMonths(1);
 
-            // Finalmente mostramos la fecha estimada de devolucion en el label
+            // Finalmente mostramos la fecha estimada de devolución en el label
             lblFechaDevolucion.Text = fecha_devolucion.ToString("dd/MM/yyyy");
         }
 
@@ -260,22 +257,18 @@ namespace sistema_gestion_biblioteca.Forms
 
         private void dgPrestamos_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            // Aqui arreglo el error de la seleccion de fila
+            // Aquí arreglo el error de la selección de fila
             if (e.RowIndex >= 0 && dgPrestamos.Rows[e.RowIndex].Cells[0].Value != null)
             {
+                // Guardar el índice de la fila seleccionada
                 index_tabla = e.RowIndex;
 
-                var fila = dgPrestamos.Rows[e.RowIndex].Cells;
-
-                cmbLibro.Text = fila[0].Value?.ToString() ?? string.Empty;
-                cmbUsuario.Text = fila[1].Value?.ToString() ?? string.Empty;
-                dtFechaInicial.Text = fila[2].Value?.ToString() ?? string.Empty;
-                lblFechaDevolucion.Text = fila[3].Value?.ToString() ?? string.Empty;
-                cmbEstadoPrestamo.Text = fila[4].Value?.ToString() ?? string.Empty;
-            }
-            else
-            {
-                index_tabla = -1;
+                // Cargar los datos seleccionados en los controles correspondientes
+                cmbLibro.Text = dgPrestamos.Rows[e.RowIndex].Cells[0].Value.ToString();
+                cmbUsuario.Text = dgPrestamos.Rows[e.RowIndex].Cells[1].Value.ToString();
+                dtFechaInicial.Value = Convert.ToDateTime(dgPrestamos.Rows[e.RowIndex].Cells[2].Value);
+                lblFechaDevolucion.Text = dgPrestamos.Rows[e.RowIndex].Cells[3].Value.ToString();
+                cmbEstadoPrestamo.Text = dgPrestamos.Rows[e.RowIndex].Cells[4].Value.ToString();
             }
         }
 
@@ -291,15 +284,15 @@ namespace sistema_gestion_biblioteca.Forms
 
         private void txtBuscar_TextChanged(object sender, EventArgs e)
         {
+            // Filtrar los préstamos basados en el texto ingresado en el cuadro de búsqueda
             FiltrarPrestamos(txtBuscar.Text);
         }
 
-        string buscarISBNPorNombreLibro(string p_nombreLibro)
+        private string buscarISBNPorNombreLibro(string nombre_libro)
         {
-            var lista_libros = obj_libro_controlador.obtenerListaLibros();
-            var libro = lista_libros.FirstOrDefault(l => l.titulo_libro == p_nombreLibro);
-
-            // Hacemos un condicion en donde se evalua que se encuentre el ISBN, si se encuentra se devuelve el ISBN, sino se retorna un string vacio
+            // Método auxiliar para obtener el ISBN de un libro a partir de su nombre
+            var listaLibros = obj_libro_controlador.obtenerListaLibros();
+            var libro = listaLibros.FirstOrDefault(l => l.titulo_libro.Equals(nombre_libro, StringComparison.OrdinalIgnoreCase));
             return libro != null ? libro.ISBN : string.Empty;
         }
     }
